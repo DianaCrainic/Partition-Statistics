1158c30de6031a6832d27184251eaca26a8581cd
Diana <dianacrainic801@yahoo.com>
2020-12-07 20:58:05 +0200

    feat: update charts

--- analyze_partition.py
+++ analyze_partition.py
@@ -12,25 +12,29 @@ def create_chart_dir_files(dictionary_of_dir_files):
     labels = list(dictionary_of_dir_files.keys())
     values = list(dictionary_of_dir_files.values())
     chart = Pie("The chart for the number of directories and files")
-    chart.set_options(labels=labels, inner_radius=0)
+    chart.set_options(labels=labels, inner_radius=0, colors=['#FFE07F', '#5AD2EC'])
     chart.add_series(values)
     return chart
 
 
 def create_chart_extension_dict(extension_dict):
+    extension_dict = dict(sorted(extension_dict.items(), key=lambda item: item[1]))
     labels = list(extension_dict.keys())
     values = list(extension_dict.values())
-    chart = Pie("The chart for the number of file extensions")
-    chart.set_options(labels=labels, inner_radius=0)
-    chart.add_series(values)
+    chart = Bar("The chart for the number of file extensions")
+    chart.set_options(labels=labels, x_label="Extensions", y_label="Number of extensions",
+                      colors=['#E09DE1' for _ in range(len(labels))])
+    chart.add_series("The number of extensions", values)
     return chart
 
 
 def create_chart_file_size_dict(file_size_dict):
+    file_size_dict = dict(sorted(file_size_dict.items(), key=lambda item: item[1]))
     labels = list(file_size_dict.keys())
     values = list(file_size_dict.values())
     chart = Bar("Bar chart")
-    chart.set_options(labels=labels, x_label="Extensions", y_label="Sum of file sizes")
+    chart.set_options(labels=labels, x_label="Extensions", y_label="Sum of file sizes",
+                      colors=['#1EAFAE' for _ in range(len(labels))])
     chart.add_series("The Sum of sizes", values)
     return chart
 
@@ -48,11 +52,12 @@ def print_number_of_elements(dictionary_of_dir_files):
 
 
 def get_extensions(extension, extensions, extension_dict):
-    extensions.add(extension)
-    if extension not in extension_dict.keys():
-        extension_dict[extension] = 1
-    else:
-        extension_dict[extension] += 1
+    if extension != '':
+        extensions.add(extension)
+        if extension not in extension_dict.keys():
+            extension_dict[extension] = 1
+        else:
+            extension_dict[extension] += 1
 
     return extensions, extension_dict
 
@@ -68,26 +73,17 @@ def print_extensions_info(extensions, extension_dict):
 
 def get_file_sizes(full_path, extension, file_size_dict):
     file_size = os.path.getsize(full_path)
-    if extension not in file_size_dict.keys():
-        file_size_dict[extension] = file_size
-    else:
-        file_size_dict[extension] += file_size
+    if extension != '':
+        if extension not in file_size_dict.keys():
+            file_size_dict[extension] = file_size
+        else:
+            file_size_dict[extension] += file_size
     return file_size_dict
 
 
 def print_file_sizes_info(file_size_dict):
     print("The dictionary of files with their sizes is: ")
     pprint.pprint(file_size_dict)
-
-
-def convert_size_for_file(size_in_bytes):
-    if size_in_bytes == 0:
-        return "0 B"
-    size_name = ("B", "KB", "MB", "GB", "TB")
-    i = int(math.floor(math.log(size_in_bytes, 1024)))
-    power = math.pow(1024, i)
-    result = round(size_in_bytes / power, 2)
-    return "%.2f %s" % (result, size_name[i])
 
 
 def partition_analysis(partition_name):
@@ -105,7 +101,7 @@ def partition_analysis(partition_name):
         dictionary_of_dir_files = {'directories': number_of_directories, 'files': number_of_files}
 
         for file_name in files:
-            extension = file_name.split(".")[-1]
+            extension = (os.path.splitext(file_name)[1][1:]).lower()
             extensions, extension_dict = get_extensions(extension, extensions, extension_dict)
 
             full_path = os.path.abspath(os.path.join(root, file_name))
